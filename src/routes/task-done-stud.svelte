27e33aaca0eae01dc1c88c8dac2a0a72f0016e42
Ural <ural2001@gmail.com>
2022-06-10 00:09:44 +0300

    make task done for student

---
+++ src/routes/task-done-stud.svelte
@@ -0,0 +1,145 @@
+<!-- GRID -->
+<div class="grid grid-1 container">
+    <!-- GRID COLUMN -->
+    <div class="grid-column">
+        <!-- STREAM BOX -->
+        <div class="stream-box big">
+            <!-- STREAM BOX VIDEO -->
+            <div class="stream-box-video" style="padding-top: 500px">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/-tTVJ3UIGUA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <!-- /STREAM BOX VIDEO -->
+
+            <!-- STREAM BOX INFO -->
+            <div class="stream-box-info" style="padding: 28px 28px 28px 28px; display: flex; justify-content: space-between">
+                <div>
+                    <!-- STREAM BOX TITLE -->
+                    <p class="stream-box-title">Название задания</p>
+                    <!-- /STREAM BOX TITLE -->
+
+                    <!-- STREAM BOX VIEWS -->
+                    <p class="stream-box-views">15 баллов</p>
+                    <p class="stream-box-views">критерий оценки</p>
+                    <!-- /STREAM BOX VIEWS -->
+                </div>
+                <div>
+                    <div class="button small" style="padding-right: 20px; padding-left: 20px; cursor: initial;">
+                        ожидает проверки
+                    </div>
+                </div>
+            </div>
+            <!-- /STREAM BOX INFO -->
+        </div>
+        <!-- /STREAM BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title">Описание:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <!-- WIDGET BOX CONTENT -->
+            <p class="widget-box-text">Описание задания</p>
+            <!-- /WIDGET BOX CONTENT -->
+        </div>
+        <!-- /WIDGET BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title mb-3">Файлы:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <!-- WIDGET BOX CONTENT -->
+            <p class="widget-box-text">1 файл</p>
+            <a class="button small" href="#" style="padding-right: 20px; padding-left: 20px">
+                скачать
+            </a>
+            <!-- /WIDGET BOX CONTENT -->
+        </div>
+        <!-- /WIDGET BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title">Решение:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <!-- WIDGET BOX CONTENT -->
+            <textarea class="mt-3" placeholder="Напишите сюда свое решение" rows="10"></textarea>
+            <!-- /WIDGET BOX CONTENT -->
+        </div>
+        <!-- /WIDGET BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title mb-3">Загрузить файлы:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <!-- WIDGET BOX CONTENT -->
+            <input type="file">
+            <!-- /WIDGET BOX CONTENT -->
+        </div>
+        <!-- /WIDGET BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title">Проверяющие:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <!-- WIDGET BOX CONTENT -->
+            <p class="widget-box-text mt-3">- Райнур Хаснов</p>
+            <!-- /WIDGET BOX CONTENT -->
+        </div>
+        <!-- /WIDGET BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title">Комментарии:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <input class="mt-3" type="text" placeholder="добавьте комментарий">
+
+            <div class="mt-3" style="padding: 10px; background-color: #f8f8fb; border-radius: 10px;">
+                <p class="widget-box-text">Райнур Хаснов: <span style="color: #a3a3a5">все отлично!</span></p>
+            </div>
+        </div>
+        <!-- /WIDGET BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title">Оценка:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <select class="mt-3" name="" id="" style="padding: 5px 10px 5px 10px">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+        </div>
+        <!-- /WIDGET BOX -->
+
+        <!-- WIDGET BOX -->
+        <div class="widget-box">
+            <!-- WIDGET BOX TITLE -->
+            <p class="widget-box-title">Статус:</p>
+            <!-- /WIDGET BOX TITLE -->
+
+            <select class="mt-3" name="" id="" style="padding: 5px 10px 5px 10px">
+                <option value="1">на доработку</option>
+                <option value="2">пройдено</option>
+                <option value="3" selected>ожидает проверки</option>
+            </select>
+        </div>
+        <!-- /WIDGET BOX -->
+
+    </div>
+    <!-- /GRID COLUMN -->
+</div>
+<!-- /GRID -->
