16882bf71e91a3846d71936f94df1b538c2e12bf
Ural <ural2001@gmail.com>
2022-06-13 10:50:22 +0300

    change course names

--- src/routes/courses.svelte
+++ src/routes/courses.svelte
@@ -29,7 +29,7 @@
             <!-- STREAM BOX INFO -->
             <div class="stream-box-info">
                 <!-- STREAM BOX TITLE -->
-                <p class="stream-box-title"><a href="profile-stream.html">Программирование на Python</a></p>
+                <p class="stream-box-title"><a href="profile-stream.html">Основы Linux</a></p>
                 <!-- /STREAM BOX TITLE -->
 
                 <!-- STREAM BOX TEXT -->
@@ -48,7 +48,7 @@
             <!-- STREAM BOX INFO -->
             <div class="stream-box-info">
                 <!-- STREAM BOX TITLE -->
-                <p class="stream-box-title"><a href="profile-stream.html">Программирование на Python</a></p>
+                <p class="stream-box-title"><a href="profile-stream.html">Веб-разработка для начинающихHTML и CSS</a></p>
                 <!-- /STREAM BOX TITLE -->
 
                 <!-- STREAM BOX TEXT -->
@@ -67,7 +67,7 @@
             <!-- STREAM BOX INFO -->
             <div class="stream-box-info">
                 <!-- STREAM BOX TITLE -->
-                <p class="stream-box-title"><a href="profile-stream.html">Программирование на Python</a></p>
+                <p class="stream-box-title"><a href="profile-stream.html">Введение в Kotlin JVM</a></p>
                 <!-- /STREAM BOX TITLE -->
 
                 <!-- STREAM BOX TEXT -->
@@ -86,7 +86,7 @@
             <!-- STREAM BOX INFO -->
             <div class="stream-box-info">
                 <!-- STREAM BOX TITLE -->
-                <p class="stream-box-title"><a href="profile-stream.html">Программирование на Python</a></p>
+                <p class="stream-box-title"><a href="profile-stream.html">Введение в Data Science и машинное обучение</a></p>
                 <!-- /STREAM BOX TITLE -->
 
                 <!-- STREAM BOX TEXT -->
@@ -105,7 +105,7 @@
             <!-- STREAM BOX INFO -->
             <div class="stream-box-info">
                 <!-- STREAM BOX TITLE -->
-                <p class="stream-box-title"><a href="profile-stream.html">Программирование на Python</a></p>
+                <p class="stream-box-title"><a href="profile-stream.html">Блокчейн</a></p>
                 <!-- /STREAM BOX TITLE -->
 
                 <!-- STREAM BOX TEXT -->
@@ -124,26 +124,7 @@
             <!-- STREAM BOX INFO -->
             <div class="stream-box-info">
                 <!-- STREAM BOX TITLE -->
-                <p class="stream-box-title"><a href="profile-stream.html">Программирование на Python</a></p>
-                <!-- /STREAM BOX TITLE -->
-
-                <!-- STREAM BOX TEXT -->
-                <p class="stream-box-text">Автор: <a href="https://www.twitch.tv/" target="_blank">Райнур Хасанов</a></p>
-                <!-- /STREAM BOX TEXT -->
-            </div>
-            <!-- /STREAM BOX INFO -->
-        </a>
-        <a href="/course" class="stream-box">
-            <!-- STREAM BOX VIDEO -->
-            <div class="stream-box-video">
-                <iframe src="img/cover/54.jpg" allowfullscreen></iframe>
-            </div>
-            <!-- /STREAM BOX VIDEO -->
-
-            <!-- STREAM BOX INFO -->
-            <div class="stream-box-info">
-                <!-- STREAM BOX TITLE -->
-                <p class="stream-box-title"><a href="profile-stream.html">Программирование на Python</a></p>
+                <p class="stream-box-title"><a href="profile-stream.html">Основы прогаммирование на Python</a></p>
                 <!-- /STREAM BOX TITLE -->
 
                 <!-- STREAM BOX TEXT -->
@@ -158,3 +139,9 @@
     <!-- /GRID -->
 </div>
 <!-- /CONTENT GRID -->
+
+<style>
+    .stream-box {
+        max-width: 300px;
+    }
+</style>
